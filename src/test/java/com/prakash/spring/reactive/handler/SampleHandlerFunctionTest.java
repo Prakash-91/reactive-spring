db454d46ddc2be88b0949baa931bc521d17893fa
Prakash Chandra Palai <prakashchandrapalai91@gmail.com>
2022-07-05 09:35:48 +0530

    Added Test Class for SampleHandlerFunction.

---
+++ src/test/java/com/prakash/spring/reactive/handler/SampleHandlerFunctionTest.java
@@ -0,0 +1,56 @@
+package com.prakash.spring.reactive.handler;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+public class SampleHandlerFunctionTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    public void fluxTest() {
+        Flux<Integer> integerFlux = webTestClient.get().uri("/functional/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(integerFlux)
+                .expectSubscription()
+                .expectNext(1)
+                .expectNext(2)
+                .expectNext(3)
+                .expectNext(4)
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void monoTest() {
+        Flux<Integer> integerFlux = webTestClient.get().uri("/functional/mono")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(integerFlux)
+                .expectSubscription()
+                .expectNext(1)
+                .verifyComplete();
+
+    }
+}
